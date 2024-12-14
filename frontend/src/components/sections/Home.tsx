1223bda70449ab7e6d7cc6b2a3e378fe79c360ac
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-12-14 23:10:07 +0530

    Home page designed

--- frontend/src/components/sections/Home.tsx
+++ frontend/src/components/sections/Home.tsx
@@ -1,7 +1,14 @@
+import LOGO from '../../../public/assets/logo/image-removebg-preview.png'
+import DownArrow from '../../../public/assets/icons/downArrow.svg'
+import Image from 'next/image';
+
+
 const Home = ()=>{
     return(
-        <div className="sections">
-            <h1 className="font-customFonts text-cgreen">Welcome to Lure</h1>
+        <div className="sections h-[800px]">
+            <div className='flex justify-center items-center h-[700px]'> <Image src={LOGO} width={300} height={200}></Image>  </div>
+
+            <div className='flex justify-center'><Image src={DownArrow} width={40} height={40}></Image> </div>
         </div>
     )
 } 
