8209ed72cb3983a73f1e5f0f4f95cb647976d50c
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-12-14 09:23:03 +0530

    Feature:Navbar done

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,29 +6,22 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 const Navbar = () => {
-    // Fix initial state to boolean false
     const [isOpen, setIsOpen] = useState(false);
-
-    // Toggle function to open/close menu
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
-
     return (
         <nav className="w-full flex justify-end absolute top-0 right-0 p-4 ">
-            {/* Menu that toggles visibility */}
-            <div className={`w-full absolute top-0 right-[50px] transform ${
-                    isOpen ? "-translate-x-[800px]" : "translate-x-full"
+            <div className={`w-full absolute top-0 right-0 transform ${
+                    isOpen ? "translate-x-0" : "translate-x-full"
                 } transition-transform duration-500 ease-in-out`}>
-                <ul className="flex w-[50%] justify-between list-none rounded-md absolute top-5 right-40">
-                    <li className="text-cblack rounded-md px-3 py-2">Home</li>
-                    <li className="text-cblack rounded-md px-3 py-2">About</li>
-                    <li className="text-cblack rounded-md px-3 py-2">Service</li>
-                    <li className="text-cblack rounded-md px-3 py-2">Contact</li>
+                <ul className="flex font-bold w-[400px] justify-between list-none bg-cblack rounded-xl shadow-2xl p-4 absolute top-1 right-[100px] ">
+                    <li className="text-cwhite rounded-md px-3 py-2 hover:text-cgreen transform hover:scale-110 hover:-translate-y-1 transition-transform duration-300 ease-in-out">Home</li>
+                    <li className="text-cwhite rounded-md px-3 py-2 hover:text-cgreen transform hover:scale-110 hover:-translate-y-1 transition-transform duration-300 ease-in-out">About</li>
+                    <li className="text-cwhite rounded-md px-3 py-2 hover:text-cgreen transform hover:scale-110 hover:-translate-y-1 transition-transform duration-300 ease-in-out">Service</li>
+                    <li className="text-cwhite rounded-md px-3 py-2 hover:text-cgreen transform hover:scale-110 hover:-translate-y-1 transition-transform duration-300 ease-in-out">Contact</li>
                 </ul>
             </div>
-
-            {/* Button to toggle menu */}
             <button className="bg-transparent border-0 absolute top-9 right-10" onClick={toggleMenu}>
                 {isOpen ? (
                     <Image src={Close} alt="Close" width={30} height={30} />
@@ -39,5 +32,4 @@ const Navbar = () => {
         </nav>
     );
 };
-
 export default Navbar;
